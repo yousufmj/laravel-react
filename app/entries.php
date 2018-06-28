7141f1f358c1069be102072bf4938ba77e08ce38
yousufmj <yousufmjaleel@gmail.com>
2018-06-28 23:33:10 +0100

    add new entries endpoint

--- app/entries.php
+++ app/entries.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class entries extends Model
+class Entries extends Model
 {
     protected $fillable = [
         'name', 'email', 'message',
