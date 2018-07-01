1f78859818aa3c4df95b3dae1a4347b141be732f
yousufmj <yousufmjaleel@gmail.com>
2018-07-01 20:37:59 +0100

    create a simple validation for input fields

--- app/Http/Controllers/ContactEntriesController.php
+++ app/Http/Controllers/ContactEntriesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Entries;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Validator;
 
 class ContactEntriesController extends Controller
 {
@@ -23,6 +24,18 @@ class ContactEntriesController extends Controller
      */
     public function create(Request $request)
     {
+        // Create a simple validation for required fields
+        $validate = Validator::make($request->all(), [
+            "name" => "required",
+            "email" => "required|email",
+            "message" => "required",
+        ]);
+
+        // return error messages
+        if ($validate->fails()) {
+            return response()->json($validate->messages(), 400);
+        }
+
         $entries = Entries::create($request->all());
 
         return response()->json($entries, 201);
